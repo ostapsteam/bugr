049046d4838c239bf99d4480dc69fc14af98e5da
Alex Korotkov <durm.icecoffee@gmail.com>
2017-07-30 01:27:05 +0300

    proposal to admin

--- bugr/bugboard/admin.py
+++ bugr/bugboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bot, TUser
+from .models import Bot, TUser, Proposal
 
 admin.site.register(Bot)
 admin.site.register(TUser)
+admin.site.register(Proposal)
