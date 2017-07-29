65e8dad8e243ab7f84e660a04c0d1d812aacaf29
Alex Korotkov <durm.icecoffee@gmail.com>
2017-07-29 12:01:13 +0300

    add profile

--- bugr/bugboard/models.py
+++ bugr/bugboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    uid = models.CharField(max_length=16, null=False, blank=False)
+    name = models.CharField(max_length=128, null=False, blank=False)
+
+    def __repr__(self):
+        return "#{} ({})".format(self.uid, self.name)
