4f272eaace651b0644f85a12a74daec46efd513d
Alex Korotkov <durm.icecoffee@gmail.com>
2017-07-30 00:45:34 +0300

    requests rendering

--- bugr/bugboard/handlers.py
+++ bugr/bugboard/handlers.py
@@ -19,29 +19,6 @@ def render_cmd(label, desc=None):
     return "{} - {}".format(label, desc if desc else DESC[label])
 
 
-@register("/help", desc="помощь")
-def help(update):
-    return "Хелпер\n\n" + "\n".join([render_cmd(x) for x in ("/my_requests", "create_request")])
-
-
-@register("/my_requests", desc="мои заявки")
-def my_requests(update):
-    count = Proposal.objects.count()
-    if count:
-        text = "Мои заявки ({})\n\n".format(count)
-        proposals = Proposal.objects.order_by("-id")[:5]
-        text += "\n".join(proposals)
-    else:
-        text = "У Вас нет актиных заявок\n\n" + "\n".join([render_cmd(x) for x in ("create_request", "/help")])
-    return text
-
-
-@register("/create_request", desc="создать заявку")
-def create_requests(update):
-    text = "Создать заявку\n\n"
-    return text
-
-
 def call(update, cmd, *args):
     if cmd not in CMD:
         return
--- bugr/bugboard/models.py
+++ bugr/bugboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 import logging
 import requests
-from .handlers import call
+from .handlers import call, render_cmd, register
 
 log = logging.getLogger(__file__)
 
@@ -83,3 +83,24 @@ class Bot(Proto):
                 return parts
 
 
+@register("/help", desc="помощь")
+def help(update):
+    return "Хелпер\n\n" + "\n".join([render_cmd(x) for x in ("/my_requests", "create_request")])
+
+
+@register("/my_requests", desc="мои заявки")
+def my_requests(update):
+    count = Proposal.objects.count()
+    if count:
+        text = "Мои заявки ({})\n\n".format(count)
+        proposals = Proposal.objects.order_by("-id")[:5]
+        text += "\n".join(proposals)
+    else:
+        text = "У Вас нет актиных заявок\n\n" + "\n".join([render_cmd(x) for x in ("create_request", "/help")])
+    return text
+
+
+@register("/create_request", desc="создать заявку")
+def create_requests(update):
+    text = "Создать заявку\n\n"
+    return text
