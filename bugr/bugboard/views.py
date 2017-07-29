a6fed585fed6046c71521c5f783601a6b438fb6c
Alex Korotkov <durm.icecoffee@gmail.com>
2017-07-29 17:22:43 +0300

    csfr

--- bugr/bugboard/views.py
+++ bugr/bugboard/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse, Http404
-from .models import Bot
-import logging
 import json
+import logging
+
+from django.http import HttpResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Bot
 
 log = logging.getLogger(__file__)
 
 
+@csrf_exempt
 def dispatch(request, botname):
     log.info("%s - call", botname)
     try:
@@ -13,5 +17,6 @@ def dispatch(request, botname):
     except Bot.DoesNotExist:
         raise Http404("Bot {} doesn't exist".format(botname))
     json_data = json.loads(request.body)
+    log.info("%s - %s", botname, json_data)
     resp = bot.handle(json_data)
     return HttpResponse(resp)
