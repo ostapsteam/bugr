7df6cf7de65d8bae0d78dac9e73aa50ba9b0de42
Alex Korotkov <durm.icecoffee@gmail.com>
2017-08-12 20:45:27 +0300

    boter import

--- bugr/bugr/urls.py
+++ bugr/bugr/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+import boter
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^boter/', include('boter.urls'))
+    url(r'^boter/', boter.urls),
 ]
